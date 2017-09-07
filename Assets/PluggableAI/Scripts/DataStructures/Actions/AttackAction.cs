cf324e9328b9b8532b0cdbac9eb164173e866ffa
oultrox <jesus-perez95@hotmail.com>
2017-09-07 17:43:45 -0300

    [Upload] Main project
    Pluggable AI using scriptable objects and a Finite State Machine.

---
+++ Assets/PluggableAI/Scripts/DataStructures/Actions/AttackAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Attack")]
+public class AttackAction : Action
+{
+    public override void Act(StateController controller)
+    {
+        Attack(controller);
+    }
+
+    private void Attack(StateController stateController)
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(stateController.eyes.position, stateController.eyes.forward.normalized * stateController.enemyStats.attackRange, Color.red);
+        if (Physics.SphereCast(stateController.eyes.position, stateController.enemyStats.lookSphereCastRadius, stateController.eyes.forward, out hit, stateController.enemyStats.attackRange)
+            && hit.collider.CompareTag("Player"))
+        {
+            if (stateController.CheckIfCountDownElapsed(stateController.enemyStats.attackRate))
+            {
+                stateController.tankShooting.Fire(stateController.enemyStats.attackForce, stateController.enemyStats.attackRate);
+            }
+        }
+    }
+}
---
+++ Assets/PluggableAI/Scripts/DataStructures/Decisions/LookDecision.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/Look")]
+public class LookDecision : Decision {
+
+    public override bool Decide(StateController controller)
+    {
+        bool targetVisible = Look(controller);
+        return targetVisible;
+    }
+
+    private bool Look(StateController controller)
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * controller.enemyStats.lookRange, Color.green);
+        if (Physics.SphereCast(controller.eyes.position, controller.enemyStats.lookSphereCastRadius, controller.eyes.forward, out hit, controller.enemyStats.lookRange)
+            && hit.collider.CompareTag("Player"))
+        {
+            controller.chaseTarget = hit.transform;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        //Para lograr hacer que gire cuando el player esté más cerca. 
+        //if (Vector3.Distance(controller.eyes.position, player.transform.position) < controller.enemystats.chaseRange)
+        //{
+
+        //}
+    }
+
+}
